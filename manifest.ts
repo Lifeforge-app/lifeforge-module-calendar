52db983e2ce7cabad75110059a59d4096c1b4ce3
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 11:59:57 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,10 +2,7 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Calendar',
-  icon: 'tabler:calendar',
   routes: {
     '/': lazy(() => import('@'))
   },
-  category: 'Productivity'
 } satisfies ModuleConfig
