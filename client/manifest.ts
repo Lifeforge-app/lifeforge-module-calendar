c62a61f9212286e50ba7264ad31d2ef6507d9dfd
Melvin Chia <melvinchia623600@gmail.com>
2026-01-11 16:28:20 +0800

    feat(client): add widgets for MiniCalendar and TodaysEvent

--- client/manifest.ts
+++ client/manifest.ts
@@ -4,5 +4,9 @@ import type { ModuleConfig } from 'shared'
 export default {
   routes: {
     '/': lazy(() => import('@'))
-  }
+  },
+  widgets: [
+    () => import('@/widgets/MiniCalendar'),
+    () => import('@/widgets/TodaysEvent')
+  ]
 } satisfies ModuleConfig
